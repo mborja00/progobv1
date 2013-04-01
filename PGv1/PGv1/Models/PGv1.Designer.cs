17e4a17da8fa578b6cfc07a4b703724247aa6ce9
Manuel Borja <mborja00@hotmail.com>
2013-04-01 17:34:32 -0500

    Agregamos la base de datos
    Agregamos la base de datos

---
+++ PGv1/PGv1/Models/PGv1.Designer.cs
@@ -0,0 +1,4 @@
+﻿// La generación de código predeterminada está deshabilitada para el modelo 'C:\Users\MANUEL\Google Drive\progobv1\PGv1\PGv1\Models\PGv1.edmx'. 
+// Para habilitar la generación de código predeterminada, cambie el valor de la propiedad del diseñador 'Estrategia de generación de código'
+// por otro valor. Esta propiedad está disponible en la ventana Propiedades cuando se abre
+// el modelo en el diseñador.
